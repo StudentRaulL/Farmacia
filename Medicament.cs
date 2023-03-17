75e85a875484a28da1627e38a1b94e1cf3e8a20a
Raul <raullucules@gmail.com>
2023-03-17 11:33:39 +0200

    Created class Medicament

---
+++ Medicament.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Medicament
+{
+	public Medicament()
+	{
+		string denumire, tip, prospect;
+		float pret;
+        //	Constructor fara parametri
+        public Medicament()
+        {
+            denumire = string.Empty;
+            pret = 0;
+        }
+
+        //	Constructor cu parametri
+        public Medicament(string _denumire, string _tip, string _prospect, float _pret)
+        {
+            denumire = _denumire;
+            tip = _tip;
+            pret = _pret;
+            prospect = _prospect:
+        }
+    }
+}
